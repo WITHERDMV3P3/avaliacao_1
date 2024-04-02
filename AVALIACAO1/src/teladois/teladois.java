9a3099e94f9a83e9627ac8edf137516045e313f3
WITHERDMV3P3 <pg9932340@gmail.com>
2024-04-02 14:18:08 -0300

    PT2

--- AVALIACAO1/src/teladois/teladois.java
+++ AVALIACAO1/src/teladois/teladois.java
@@ -46,7 +46,7 @@ public class teladois extends JFrame {
 	public teladois() {
 		setExtendedState(Frame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1308, 784);
+		setBounds(100, 100, 1800, 784);
 		contentPane = new JPanel();
 		contentPane.setBorder(new CompoundBorder());
 
@@ -54,31 +54,31 @@ public class teladois extends JFrame {
 		contentPane.setLayout(null);
 		
 		scrollBar = new JScrollBar();
-		scrollBar.setBounds(1275, 73, 17, 594);
+		scrollBar.setBounds(1326, 79, 17, 609);
 		contentPane.add(scrollBar);
 		
 		table = new JTable();
-		table.setBounds(10, 73, 1282, 594);
+		table.setBounds(10, 79, 1333, 609);
 		contentPane.add(table);
 		
 		btnVoltar = new JButton("VOLTAR");
 		btnVoltar.setFont(new Font("Arial", Font.BOLD, 14));
-		btnVoltar.setBounds(10, 20, 122, 42);
+		btnVoltar.setBounds(36, 26, 122, 42);
 		contentPane.add(btnVoltar);
 		
 		btnDetalhes = new JButton("DETALHES");
 		btnDetalhes.setFont(new Font("Arial", Font.BOLD, 14));
-		btnDetalhes.setBounds(856, 20, 122, 42);
+		btnDetalhes.setBounds(882, 26, 122, 42);
 		contentPane.add(btnDetalhes);
 		
 		btnDeletar = new JButton("DELETAR");
 		btnDeletar.setFont(new Font("Arial", Font.BOLD, 14));
-		btnDeletar.setBounds(998, 20, 122, 42);
+		btnDeletar.setBounds(1024, 26, 122, 42);
 		contentPane.add(btnDeletar);
 		
 		btnAlterar = new JButton("ALTERAR");
 		btnAlterar.setFont(new Font("Arial", Font.BOLD, 14));
-		btnAlterar.setBounds(1138, 20, 122, 42);
+		btnAlterar.setBounds(1164, 26, 122, 42);
 		contentPane.add(btnAlterar);
 	}
 }
