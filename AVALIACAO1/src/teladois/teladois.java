b2de833ba5dcf23f0c7dc279ec9c65b0836e83b7
WITHERDMV3P3 <pg9932340@gmail.com>
2024-04-02 14:06:48 -0300

    teladois

--- AVALIACAO1/src/teladois/teladois.java
+++ AVALIACAO1/src/teladois/teladois.java
@@ -8,11 +8,15 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.JScrollBar;
 
 public class teladois extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
+	private JScrollBar scrollBar;
 
 	/**
 	 * Launch the application.
@@ -36,12 +40,19 @@ public class teladois extends JFrame {
 	public teladois() {
 		setExtendedState(Frame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1301, 756);
+		setBounds(100, 100, 1308, 784);
 		contentPane = new JPanel();
 		contentPane.setBorder(new CompoundBorder());
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		scrollBar = new JScrollBar();
+		scrollBar.setBounds(1311, 73, 17, 594);
+		contentPane.add(scrollBar);
+		
+		table = new JTable();
+		table.setBounds(46, 73, 1282, 594);
+		contentPane.add(table);
 	}
-
 }
